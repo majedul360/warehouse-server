6d5c3e2c74a1139e23593a1a4be71195da9acf4b
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-04-29 16:20:58 +0600

    connect with database done

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,6 +12,21 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("hellow world");
 });
+
+// connect with database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1y2cc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  await client.connect();
+  console.log("successful");
+};
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log("server number", port);
