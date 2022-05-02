da78ea4798317fd8ed938ef7fa566847e262e780
Mr. Majedul Islam <majedulislam393@gmail.com>
2022-05-02 12:33:20 +0600

    again retry

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
+const port = process.env.PORT || 5000;
+// middle ware
+app.use(cors());
+app.use(express.json());
 
 // middleware
 app.use(cors());
